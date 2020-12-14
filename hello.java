13e1674e176cdc934cbd943ffe1cadbd95f8f663
salmannoushad <salmansnoushad@gmail.com>
2020-12-14 23:59:21 +0600

    added a new method called greeting to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
     public static void main(string[] args){
         System.out.println("Vs code is Awesome");
+
+        greeting("Salman Noushad");
+    }
+
+    public static void greeting(string name){
+        System.out.println("welcome " + name);
     }
 }

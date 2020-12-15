f2a2129e64b3c696bb960f25f2c6c423258825a5
salmannoushad <salmansnoushad@gmail.com>
2020-12-15 18:20:23 +0600

    all method removed and message method

--- hello.java
+++ hello.java
@@ -2,16 +2,9 @@ public class hello{
     public static void main(string[] args){
         System.out.println("Vs code is Awesome");
 
-        greeting("");
+   
     }
-
-    public static void greeting(){
-        System.out.println("hello hello");
-     }
-    public static int sqr(int x){
-        return x;
-    }
-    public static void isow(){
-
-    };
+public static void message(){
+    System.out.println("Hurrey!! now we are able to contribute open source...");
+}
 }
